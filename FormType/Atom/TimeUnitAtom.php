b867045a15d438cce0d4618b397ef79939d90470
lighthart <dlighthart@mesd.k12.or.us>
2014-06-03 13:26:08 -0700

    Updated to have clear boxes

--- FormType/Atom/TimeUnitAtom.php
+++ FormType/Atom/TimeUnitAtom.php
@@ -5,18 +5,15 @@ namespace Mesd\Standard\UnitBundle\FormType\Atom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
 
 class TimeUnitAtom extends AbstractType
 {
 
     private $em;
-    private $user;
-
 
     public function __construct( $em, $securityContext ) {
         $this->em=$em;
-        $this->user=$securityContext->getToken()->getUser();
-
     }
 
     public function buildForm( FormBuilderInterface $builder, array $options ) {
@@ -28,24 +25,48 @@ class TimeUnitAtom extends AbstractType
      * @param object  $resolver
      */
     public function setDefaultOptions( OptionsResolverInterface $resolver ) {
-        $user = $this->user;
+        $attr = function( Options $options ) {
+
+            $default =
+                array(
+                'class'                           => 's2'            ,
+                'data-select2-minimuminputlength' => 0               ,
+                'data-select2-limit'              => 10              ,
+                'data-select2-placeholder'        => 'Select Unit'   ,
+                'data-select2-allowclear'         => true            ,
+            );
+
+            if ( isset( $options['new_attr'] ) ) {
+                return array_replace( $default, $options['new_attr'] );
+            } else {
+                return $default;
+            }
+
+        };
+
         $resolver->setDefaults(
             array(
-                'class'           => 'Mesd\Standard\UnitBundle\Entity\Unit'
-                , 'label'         => 'Unit'
-                , 'label_attr'    => array( 'class' => 'txtl' )
-                , 'data_class'    => null
-                , 'empty_value'   => ''
-                , 'query_builder' =>
-                function ( $repository ) use ( $user ) {
+                'class'         => 'Mesd\Standard\UnitBundle\Entity\Unit' ,
+                'attr'          => $attr                                  ,
+                'new_attr'      => array()                                ,
+                'label'         => 'Unit'                                 ,
+                'label_attr'    => array( 'class' => 'txtl' )             ,
+                'data_class'    => null                                   ,
+                'empty_value'   => ''                                     ,
+                'query_builder' =>
+                function ( $repository ) {
                     $qb = $repository->createQueryBuilder( 'u' )
-                    ->where('u.time = true')
-                    ->addOrderBy( 'u.shortName' );
+                    ->addOrderBy( 'u.shortName' )
+                    ->where( 'u.time = true' );
 
                     return $qb;
                 }
+
             )
         );
+
+        $resolver->setOptional( array( 'new_attr' ) );
+
     }
 
     public function getParent() {
